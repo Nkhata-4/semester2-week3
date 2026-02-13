acc179af41165c6fef6bd279e00da0d7389af905
Nkhata-4 <YanjaniNkhata08@outlook.com>
2026-02-13 11:50:23 +0000

    worked on portfolio task

--- worksheet/portfolio/salary.c
+++ worksheet/portfolio/salary.c
@@ -1,10 +1,10 @@
 
 #include <stdio.h>
-
+#include <math.h>
 /*
  * Potrfolio submission
- * Name:
- * ID:
+ * Name: Yanjani Nkhata
+ * ID: 201930814
  */
 
  int main( void ) {
@@ -18,6 +18,22 @@
     //printf("NI contribution £...",var_name);
     //printf("Tax contribution £...",var_name);
     //printf("Take home salary £...",var_name);
+
+    float salary = 36250; //14000-40000
+    float NI = 0.08 ; //0%-10%
+    float tax = 0.15; //10%-30%
+    
+    
+    float NI_contribution = salary * NI;
+    float post_NI = salary - NI_contribution;
+    float up_for_tax = post_NI - 12500;
+    float tax_contribution = up_for_tax * tax;
+    float take_home_sal = post_NI - (up_for_tax * tax);
+
+    printf("Salary £%.2f\n",salary);
+    printf("NI contribution: £%.2f\n",NI_contribution);
+    printf("Tax contribution: £%.2f\n",tax_contribution);
+    printf("Take home salary: £%.2f\n",take_home_sal);
 
     return 0;
  }
